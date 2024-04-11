845b7ed1cd07e2a6423c5b6ae797f86534b821eb
Igorg9865 <igor.gavric111@gmail.com>
2024-04-11 10:51:19 +0200

    forward pass

--- forward_backward.py
+++ forward_backward.py
@@ -30,14 +30,46 @@ class Model:
       self.weights1 = self.layer1.get_weights()
       self.layer2 = Layer([1,2], 2, 0.35)
    
-   def forward(self):
-      x = self.layer1.inp
-      y = [0.1, 0.3]
-      h1 = dotproduct(x, y) + self.layer1.bias
-      print(h1)
+   def forward(self, layer):
+      inp = layer.inp
+      weights = layer.get_weights()
 
-
+      w1 = []
+      for i in range(0, len(weights) - 2):
+         if i == 0: w1.append(weights[i])
+         else: w1.append(weights[i + 1])
+      w2 = []
+      for i in range(1, len(weights) - 1):
+         if i == 1: w2.append(weights[i])
+         else: w2.append(weights[i + 1])
+      
+      h1 = dotproduct(inp, w1) + layer.bias
+      output1 = sigmoid(h1)
+      h2 = dotproduct(inp, w2) + layer.bias
+      output2 = sigmoid(h2)
+      print(output1, output2)
 
 nn = Model()
+nn.forward(nn.layer1)
 
-nn.forward()
+w = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8]
+w1 = []
+for i in range(0,2):
+   if i == 0: w1.append(w[i]) 
+   else: w1.append(w[i + 1])
+#print(w1)
+w2 = []
+for i in range(1,3):
+   if i == 1: w2.append(w[i]) 
+   else: w2.append(w[i + 1])
+#print(w2)
+w3 = []
+for i in range(4,6):
+   if i == 4: w3.append(w[i]) 
+   else: w3.append(w[i + 1])
+#print(w3)
+w4 = []
+for i in range(5,7):
+   if i == 5: w4.append(w[i]) 
+   else: w4.append(w[i + 1])
+#print(w4)
