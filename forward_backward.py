861f654b1f2ba073eafbfdeac8e612835ed66d6d
Igorg9865 <igor.gavric111@gmail.com>
2024-04-11 09:32:46 +0200

    initial forward pass

--- forward_backward.py
+++ forward_backward.py
@@ -4,13 +4,18 @@ def sigmoid(x):
    out = 1 / (1 + math.exp(-x))
    return out
 
+def dotproduct(x,y):
+   out = 0
+   for i in range(len(x)):
+      out += x[i] * y[i]
+   return out
+
 class Layer:
    def __init__(self, inp, num_nodes, bias):
       self.inp = inp
       self.num_inp = len(inp)
       self.num_nodes = num_nodes
       self.bias = bias       
-    #  self.weights = get_weights(self.num_inp, self.num_nodes)
 
    def get_weights(self):
       weights = []
@@ -19,10 +24,20 @@ class Layer:
          weights.append(0.1 * i + 0.1)
       return weights
 
+class Model:
+   def __init__(self):
+      self.layer1 = Layer([0.1, 0.5], 2, 0.25)
+      self.weights1 = self.layer1.get_weights()
+      self.layer2 = Layer([1,2], 2, 0.35)
    
-layer1 = Layer([1,2], 2, 0.25)
+   def forward(self):
+      x = self.layer1.inp
+      y = [0.1, 0.3]
+      h1 = dotproduct(x, y) + self.layer1.bias
+      print(h1)
 
 
 
+nn = Model()
 
-
+nn.forward()
