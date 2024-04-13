70f675c1af957c6897e3d9121ffd9e1531f991b3
Igorg9865 <igor.gavric111@gmail.com>
2024-04-13 16:36:39 +0200

    create function class for mlops and apply fxn

--- mlops.py
+++ mlops.py
@@ -1,18 +1,32 @@
 import math
 
+class Function:
+   def __init__(self, x):
+      self.x = x
 
-class Sigmoid:
+   def apply(fxn, x, state:int):
+      if state == 1: fxn = forward
+      else: fxn = backward
+      return fxn(x)
+
+
+class Sigmoid(Function):
    def forward(x): return 1/ (1 + math.exp(-x))
    def backward(x): 
       sigma = 1 / (1 + math.exp(-x))
       return sigma * (1 - sigma)
-
-def sigmoid(x):
-   out = 1 / (1 + math.exp(-x))
-   return out
 
 def dotproduct(x,y):
    out = 0
    for i in range(len(x)):
       out += x[i] * y[i]
    return out
+
+a = Tensor(1)
+print(a.create())
+
+x = 1
+
+a.add(x) == 2
+
+F.Sigmoid.apply(a)
