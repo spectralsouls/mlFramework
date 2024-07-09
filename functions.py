3175e50233ae172eca13d8a01ef7694643a3374b
Igorg9865 <igor.gavric111@gmail.com>
2024-07-09 23:51:11 +0200

    added context for functions

--- functions.py
+++ functions.py
@@ -3,36 +3,39 @@ import numpy as np
 
 # Unary Fxns
 class Negative(Function):
-    def forward(x): return np.negative(x)
+    def forward(self, x): return np.negative(x)
 
 class Reciprocal(Function):
-    def forward(x): 
+    def forward(self, x): 
         x_casted = np.array(x, dtype=np.float32)
         return np.reciprocal(x_casted)
 
 class Sqrt(Function):
-    def forward(x): return np.sqrt(x)
+    def forward(self, x): return np.sqrt(x)
 
 class Exp(Function):
-    def forward(x): return np.exp(x)
+    def forward(self, x): return np.exp(x)
 
 class Log(Function):
-    def forward(x): return np.log(x)
+    def forward(self, x): return np.log(x)
 
 class Sin(Function):
-    def forward(x): return np.sin(x)
+    def forward(self, x): return np.sin(x)
 
 class Relu(Function):
-    def forward(x): return np.maximum(x, 0)
+    def forward(self, x): return np.maximum(x, 0)
 
 class Sigmoid(Function):
-    def forward(x): 
+    def forward(self, x): 
         denom = np.add(1, np.exp(np.negative(x)))
         return np.divide(1, denom)
 
 # Binary Fxns
 class Add(Function):
-    def forward(x, y): return np.add(x,y) 
+    def forward(self, x, y): return np.add(x,y) 
+
+class Sub(Function):
+    def forward(self, x, y): return x + -(y)
 
 class Mul(Function):
-    def forward(x, y): return np.multiply(x, y)
+    def forward(self, x, y): return np.multiply(x, y)
--- tensor.py
+++ tensor.py
@@ -1,30 +1,37 @@
 from __future__ import annotations
 import numpy as np
+from typing import Union, List, Tuple
 
 # np.frombuffer vs np.array
-
 class Function:
+    def __init__(self, *x:tensor):
+         self.parents = x
+
     def forward(self, *args): raise NotImplementedError(f"forward not implemented for {type(self)}")
-    def backward(self): raise NotImplementedError(f"backward not implemented for {type(self)}")
+    def backward(self, *args): raise NotImplementedError(f"backward not implemented for {type(self)}")
 
     @classmethod
-    def apply(fxn, *x:tensor):
-        ret = fxn.forward(*[t.data for t in x])
-        return tensor(ret)
+    def apply(fxn:Function, *x:tensor) -> tensor:
+        ctx = fxn(*x)
+        ret = tensor(ctx.forward(*[t.data for t in x]))
+        ret.ctx = ctx
+        return ret
 
 import functions as F
 
-def broadcasted(x):
+def broadcasted(x) -> tensor:
         if not isinstance(x, tensor):
             x = tensor(x)
         return x
 
 class tensor:
-    def __init__(self, data, dtype=np.int32):
+    def __init__(self, data:Union[List, Tuple], dtype=np.int32): # data takes constants too
+        self.ctx = None
         self.data, self.dtype = data, dtype
         self.shape = np.array(data, dtype).shape
         self.size = () if len(self.shape) == 0 else len(data)
 
+    
     def numpy(self): return np.array(self.data)
 
     def negative(self): return F.Negative.apply(self)
@@ -37,12 +44,12 @@ class tensor:
     def sigmoid(self): return F.Sigmoid.apply(self)
 
     def add(self, x): return F.Add.apply(self, broadcasted(x))
-    def sub(self, x): return F.Add.apply(self, broadcasted(-x))
+    def sub(self, x): return F.Sub.apply(self, broadcasted(x))
     def mul(self, x): return F.Mul.apply(self, broadcasted(x))
-    def div(self, x): return F.Mul.apply(self, broadcasted(x).recip())
+    def div(self, x): return F.Mul.apply(self, broadcasted(x).recip()) # should there be a Div function?
 
     def __repr__(self):
-        return f"{self.data}"
+        return f"tensor({self.data})"
     def __getitem__(self, idx): 
         return np.array(self.data)[idx]
     def __neg__(self): return self.negative()
