405c0d35e3d1545d8ff3403fb1bdce41d6522f04
Igorg9865 <igor.gavric111@gmail.com>
2024-07-16 16:03:36 +0200

    train function works

--- examples/mnist.py
+++ examples/mnist.py
@@ -1,8 +1,10 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torchvision import datasets
+from torchvision import datasets, transforms
+import torch.optim as optim
 import functools
+
 
 torch.manual_seed(42)
 
@@ -11,21 +13,27 @@ def sequential(inp, layers):
 
 class Model(nn.Module):
     def __init__(self):
+        super(Model, self).__init__()
         self.layers = [
-            nn.Conv2d(1, 32, 3, 1), F.relu,
-            nn.Conv2d(32, 64, 3, 1), F.relu,
-            lambda x: F.max_pool2d(x, kernel_size=(2,2)), nn.Dropout(0.25),
-            torch.flatten, nn.Linear(9216, 128),
-            F.relu, nn.Dropout(0.5), 
-            nn.Linear(128, 10), 
-            lambda x: F.log_softmax(x, dim=0)
+            nn.Conv2d(1, 32, 5), F.relu,
+            nn.Conv2d(32, 32, 5), F.relu,
+            nn.BatchNorm2d(32), lambda x: F.max_pool2d(x, (2,2)), 
+            nn.Conv2d(32, 64, 3), F.relu, 
+            nn.Conv2d(64, 64, 3), F.relu,
+            nn.BatchNorm2d(64), lambda x: F.max_pool2d(x, (2,2)),
+            lambda x: torch.flatten(x, start_dim=1), nn.Linear(576, 10)
                       ]
-            
+                 
     def __call__(self, x): return sequential(x, self.layers)
-        
+
 model = Model()
-inp = torch.rand(size=[1, 28, 28])
+#adam = optim.Adam(model.parameters)
 
-a = sequential(inp, model.layers)
-print(a)
-
+def train_step(bs=512):
+    #adam.zero_grad()
+    samples = torch.randint(low=0, high=60000, size=(bs,)) 
+    loss = F.cross_entropy(model(x_train[samples].float()), y_train[samples])#.backward()
+    print(f"loss: {loss: 4.2f}")
+    loss.backward()
+    #adam.step()
+    
